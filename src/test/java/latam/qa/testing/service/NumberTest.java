12f5f2cdca055d504a48eade78b77dee641bd7d6
cpontigo <c.pontigogil@uandresbello.edu>
2016-12-05 03:08:11 -0300

    2b
    2b

--- src/test/java/latam/qa/testing/service/NumberTest.java
+++ src/test/java/latam/qa/testing/service/NumberTest.java
@@ -16,8 +16,24 @@ public class NumberTest {
     public void creatingString_twoSimpleNumbers_mustFail(){
         Number n = new Number();
         String result = n.stringCalc(1, 2);
-        assertNotSame(String.format("%d,%d", 1, 5), result);
+        assertFalse(String.format("%d,%d", 1, 5), result);
 
     }
+	@Test
+	public void divideNumeros_cero(){
+		Number n = new Number();
+		int x=5,y=0;
+		Int result = n.intDivideNumeros(x,y);
+		if(y==0) fail();
+		
+	}
+	
+	@Test
+	public void divideNumeros(){
+		Number n = new Number();
+		Int result = n.intDivideNumeros(5,10);
+		//assertEquals
+		
+	}
    
 }
